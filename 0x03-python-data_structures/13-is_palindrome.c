2a1b2a8c1132ab63f30d1f17fbb7001d2c3b601e
Mehdisaber332 <mehdisaber332@gmail.com>
2023-09-11 13:50:32 -0700

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,22 +1,41 @@
-#include"lists.h"
-#include <stdlib.h>
+#include "lists.h"
+#include <stddef.h> /* Include for NULL */
 
+/**
+ * is_palindrome_recursive - recursively checks if a linked list is a palindrome
+ * @left: pointer to the left node
+ * @right: pointer to the right node
+ *
+ * Return: 1 if it is a palindrome, 0 otherwise
+ */
+int is_palindrome_recursive(listint_t **left, listint_t *right)
+{
+	if (right == NULL)
+		return (1);
+
+	int is_palindrome = is_palindrome_recursive(left, right->next);
+
+	if (is_palindrome == 0)
+		return (0);
+
+	if ((*left)->n != right->n)
+		return (0);
+
+	*left = (*left)->next;
+
+	return (1);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to the head of the linked list
+ *
+ * Return: 1 if it is a palindrome, 0 otherwise
+ */
 int is_palindrome(listint_t **head)
 {
-    listint_t *current = *head;
-    static listint_t *temp;
+	if (head == NULL || *head == NULL)
+		return (1);
 
-    if (current == NULL)
-        return (1);
-
-    if (temp == NULL)
-        temp = current;
-
-    if (is_palindrome(&current->next) && temp->n == current->n)
-    {
-        temp = temp->next;
-        return (1);
-    }
-    else
-        return (0);
+	return (is_palindrome_recursive(head, *head));
 }
