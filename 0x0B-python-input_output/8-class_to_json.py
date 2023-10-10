c15b43ae2a17f4d48f92afe4bca94f697ad6e68a
Mehdisaber332 <133115021+Mehdisaber332@users.noreply.github.com>
2023-10-10 17:21:30 +0100

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""Module for class_to_json"""
+"""This module defines a Python class-to-JSON function"""
 
 
 def class_to_json(obj):
-    """returns the dictionary description with simple data structure
-    (list, dictionary, string, integer and boolean) for JSON serialization"""
-
+    """Returns the dictionary representation of a simple data structure"""
     return obj.__dict__
-
