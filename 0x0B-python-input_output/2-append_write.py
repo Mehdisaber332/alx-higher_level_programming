e3ee194cf8336279d3a0b903484d48c1deac0547
Mehdisaber332 <133115021+Mehdisaber332@users.noreply.github.com>
2023-10-10 17:15:37 +0100

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-""" Module that returns a list of lists of integers representing Pascal's"""
+"""This module defines a file-appending function."""
 
 
-def pascal_triangle(n):
-    """returns a list of lists of integers representing the Pascal's"""
-    my_list = []
-    for i in range(n):
-        my_list.append([])
-        for j in range(i+1):
-            try:
-                if j - 1 == -1:
-                    my_list[i].append(1)
-                else:
-                    my_list[i].append(my_list[i-1][j-1] + my_list[i-1][j])
-            except IndexError:
-                my_list[i].append(1)
-    return (my_list)
-
+def append_write(filename="", text=""):
+    """Appends a string to the end of a UTF8 text file
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
