3179b6dd76c535b0b646a6d99807af07c50ea652
Mehdisaber332 <133115021+Mehdisaber332@users.noreply.github.com>
2023-10-10 17:28:49 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,6 @@
+7-add_item.py
 #!/usr/bin/python3
-"""This module adds all arguments to a Python list and save them to a file."""
-
-
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
 if __name__ == "__main__":
