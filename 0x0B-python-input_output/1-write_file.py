79011fedc54510f2d2bd9a2de749b397b79c7b3a
Mehdisaber332 <133115021+Mehdisaber332@users.noreply.github.com>
2023-10-10 17:16:22 +0100

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Module for write_file"""
+"""This module defines a file-writing function."""
 
 
 def write_file(filename="", text=""):
-    """Write a string to a text file (UTF8)"""
-    with open(filename, 'w', encoding="utf-8") as f:
+    """Writes a string to a UTF8 text file
+    """
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
-
