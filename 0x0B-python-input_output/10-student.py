a48fa0b24a550c4bb696e7d94d6051e8c79ef4ef
Mehdisaber332 <133115021+Mehdisaber332@users.noreply.github.com>
2023-10-10 17:22:08 +0100

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""class Student that defines a student by:"""
+"""This module defines a class Student"""
 
 
 class Student:
-    """class Student that defines a student by:"""
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
-        """Public instance attributes:"""
+        """Initializes a new Student
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """that retrieves a dictionary representation"""
-        if attrs is None:
-            return self.__dict__
-        else:
-            my_dict = {}
-            for i in attrs:
-                if hasattr(self, i):
-                    my_dict[i] = getattr(self, i)
-            return my_dict
-
+        """Get a dictionary representation of the Student
+        If attrs is a list of strings, represents only those attributes
+        included in the list
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
